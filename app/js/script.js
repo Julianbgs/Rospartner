36b49da95d40952812cece7019d784c6e59ec496
Blagis <julianbgs96@gmail.com>
2019-01-12 14:06:38 +0300

    finish scripts

--- app/js/script.js
+++ app/js/script.js
@@ -5,4 +5,29 @@ $(document).ready(function () {
         $('#field').toggleClass('main__border');
         $('#label').toggleClass('main__color');
     });
+
+    //close script
+
+    //active element
+
+    $(document).on('click', '#Telegram', function (event) {
+        $('#telegram-gray').toggleClass('grayscale');
+        $('#telegram-dot').toggleClass('dot');
+    });
+
+    $(document).on('click', '#ICQ', function (event) {
+        $('#icq-gray').toggleClass('grayscale');
+        $('#icq-dot').toggleClass('dot');
+    });
+
+    $(document).on('click', '#Skype', function (event) {
+        $('#skype-gray').toggleClass('grayscale');
+        $('#skype-dot').toggleClass('dot');
+    });
+
+    $(document).on('click', '#Slack', function (event) {
+        $('#slack-gray').toggleClass('grayscale');
+        $('#slack-dot').toggleClass('dot');
+    });
+
 });
